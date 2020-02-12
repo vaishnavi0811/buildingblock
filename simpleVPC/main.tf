f25d1250367a431592df2c083e5bee3863b9dff8
rajagopalanrg <56252656+rajagopalanrg@users.noreply.github.com>
2020-02-12 21:27:32 +0530

    change tag type

--- simpleVPC/main.tf
+++ simpleVPC/main.tf
@@ -20,7 +20,7 @@ resource "aws_vpc" "main" {
     enable_dns_hostnames = true
     enable_dns_support   = true
     instance_tenancy     = "default"
-    tags  {
+    tags  = {
       Name = var.vpcname
     }
 }
