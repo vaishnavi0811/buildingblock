e04f0eaf9e1d44ec7d5f58d6863c9d2144161175
rajagopalanrg <56252656+rajagopalanrg@users.noreply.github.com>
2020-02-12 21:23:06 +0530

    added access key

--- simpleVPC/main.tf
+++ simpleVPC/main.tf
@@ -4,6 +4,17 @@ variable "vpc_cidr_block"{
 variable "vpcname"{
     type = string
 }
+variable "access_key"{
+    type = string
+}
+variable "secret_access_key"{
+    type = string
+}
+provider "aws" {
+  region     = "us-east-1"
+  access_key = var.access_key
+  secret_key = var.secret_access_key
+}
 resource "aws_vpc" "main" {
     cidr_block           = var.vpc_cidr_block
     enable_dns_hostnames = true
