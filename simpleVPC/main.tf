b287e8f5cecb20f82f8d3b7436dff242df96ff91
rajagopalanrg <56252656+rajagopalanrg@users.noreply.github.com>
2020-02-13 09:19:46 +0530

    removed tags

--- simpleVPC/main.tf
+++ simpleVPC/main.tf
@@ -20,7 +20,4 @@ resource "aws_vpc" "main" {
     enable_dns_hostnames = true
     enable_dns_support   = true
     instance_tenancy     = "default"
-    tags  = {
-      Name = var.vpcname
-    }
 }
