71011c076e3ff774550b45197a5b8f9789246424
rajagopalanrg <56252656+rajagopalanrg@users.noreply.github.com>
2020-02-13 21:58:25 +0530

    removed tags from vpc

--- simpleVPC/main.tf
+++ simpleVPC/main.tf
@@ -23,9 +23,7 @@ resource "aws_vpc" "main" {
   enable_dns_hostnames = true
   enable_dns_support   = true
   instance_tenancy     = "default"
-  tags = {
-    Name = "sentinelTest"
-  }
+  
 }
 resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
