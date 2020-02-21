4fbbaf789926a129cfd0a5c60ec3ae2d7e9a08e9
Rajagopalan Ramanujam <Rajagopalan.Ramanujam@ey.com>
2020-02-21 17:11:11 +0530

    added target group

--- apploadbalancer/loadbalancer/main.tf
+++ apploadbalancer/loadbalancer/main.tf
@@ -1,11 +1,11 @@
 variable "certARN" {
-    type = string
+  type = string
 }
 
 data "aws_vpc" "getVPC" {
-  filter{
-      name = "tag:Name"
-      values = ["testvpctoimport"]
+  filter {
+    name   = "tag:Name"
+    values = ["testvpctoimport"]
   }
 }
 data "aws_subnet" "getSubnet" {
@@ -13,22 +13,32 @@ data "aws_subnet" "getSubnet" {
 }
 
 resource "aws_lb" "MyAWSResource" {
-    name = "sentinelELB"
-    internal = false
-    load_balancer_type = "application"
-    security_groups = ["sg-001d4d1052759f3d5"]
-    subnets = [data.aws_subnet.getSubnet.id]
-    enable_deletion_protection = true
-    enable_http2 = true
-    tags = {
-        Name = "sentinelELB"
-        Environment = "Development"
-    }
+  name                       = "sentinelELB"
+  internal                   = false
+  load_balancer_type         = "application"
+  security_groups            = ["sg-001d4d1052759f3d5"]
+  subnets                    = [data.aws_subnet.getSubnet.id]
+  enable_deletion_protection = true
+  enable_http2               = true
+  tags = {
+    Name        = "sentinelELB"
+    Environment = "Development"
+  }
+}
+resource "aws_lb_target_group" "albtargetgroup" {
+  name     = "albfrontend"
+  port     = 443
+  protocol = "HTTPS"
+  vpc_id   = data.aws_vpc.getVPC.id
 }
 resource "aws_lb_listener" "frontend" {
   load_balancer_arn = aws_lb.MyAWSResource.arn
-  port = "443"
-  protocol = "HTTPS"
-  ssl_policy = "ELBSecurityPolicy-FS-2018-06"
-  certificate_arn = var.certARN
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-FS-2018-06"
+  certificate_arn   = var.certARN
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.albtargetgroup.arn
+  }
 }
